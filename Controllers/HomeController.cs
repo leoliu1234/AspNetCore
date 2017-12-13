5d2c52e890a633f8e19bb3c7e5f9a73395322caf
leoliu123456 <leo---liu@hotmail.com>
2017-12-13 23:04:39 +0800

    Add log

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace demo
 {
     public class HomeController : Controller
     {
         private readonly IUserLogic _userLogic;
+        private readonly ILogger _logger;
 
-        public HomeController(IUserLogic userLogic)
+        public HomeController(
+            IUserLogic userLogic,
+            ILogger logger
+            )
         {
             _userLogic = userLogic;
+            _logger = logger;
         }
 
         public ActionResult Index()
         {
+            _logger.LogError(1,null,"test");
 
             return View();
         }
--- Startup.cs
+++ Startup.cs
@@ -5,30 +5,50 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace demo
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+                // Configuration = new ConfigurationBuilder()
+                // .SetBasePath(env.ContentRootPath)
+                // .AddJsonFile("appsettings.json",optional:false,reloadOnChange: true)
+                // .Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            
             services.AddSingleton<IUserLogic, UserLogic>();
+
+            services.AddTransient<ILogger, Logger>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             //  app.Run(async context =>
             // {
             //     await context.Response.WriteAsync("Hello from 2nd delegate.");
             // });
+            loggerFactory.AddDebug(LogLevel.Warning);
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+
             app.UseStaticFiles();
-            app.UseMvc((routes)=>{
+            app.UseMvc((routes) =>
+            {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}"
