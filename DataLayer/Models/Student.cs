b855c19443b91aa624a7fa88acdb6c6c47066797
T04395 <Leo.Liu@augmentum.com.cn>
2017-12-15 12:25:10 +0800

    Add EF

---
+++ DataLayer/Models/Student.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace AspNetCore.DataLayer.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
